128648662963d383ced070b3edb4a7270572d79a
Adam Debus <adam@debus.net>
2026-02-16 22:59:29 -0800

    Normalized GameOwner into Game

--- src/crud/game.py
+++ src/crud/game.py
@@ -2,30 +2,31 @@ from collections.abc import Sequence
 
 from sqlmodel import Session, select
 
-from ..models import game, gameowner, player
+from ..models import game as model_game
+from ..models import player as model_player
 
 
-def get_game_by_name(session: Session, name: str) -> game.Game | None:
-    statement = select(game.Game).where(game.Game.name == name)
+def get_game_by_name(session: Session, name: str) -> model_game.Game | None:
+    statement = select(model_game.Game).where(model_game.Game.name == name)
     return session.exec(statement).first()
 
 
-def get_all_games(session: Session) -> Sequence[game.Game] | None:
-    statement = select(game.Game)
+def get_all_games(session: Session) -> Sequence[model_game.Game] | None:
+    statement = select(model_game.Game)
     return session.exec(statement).all()
 
 
-def create_game(session: Session, game_in: game.GameCreate) -> game.Game:
-    db_game = game.Game.model_validate(game_in)
+def create_game(session: Session, game_in: model_game.GameCreate) -> model_game.Game:
+    db_game = model_game.Game.model_validate(game_in)
     session.add(db_game)
     session.commit()
     session.refresh(db_game)
     return db_game
 
 
-def create_game_owner(session: Session, game_id: int, player_id: int) -> gameowner.GameOwner:
+def create_game_owner(session: Session, game_id: int, player_id: int) -> model_game.GameOwner:
 
-    db_game_owner = gameowner.GameOwner(game_id=game_id, owner_id=player_id)
+    db_game_owner = model_game.GameOwner(game_id=game_id, owner_id=player_id)
     session.add(db_game_owner)
     session.commit()
     session.refresh(db_game_owner)
@@ -34,26 +35,26 @@ def create_game_owner(session: Session, game_id: int, player_id: int) -> gameown
 
 def check_game_owner_exists(session: Session, owner_id: int, game_id: int) -> bool:
     statement = (
-        select(gameowner.GameOwner)
-        .where(gameowner.GameOwner.owner_id == owner_id)
-        .where(gameowner.GameOwner.game_id == game_id)
+        select(model_game.GameOwner)
+        .where(model_game.GameOwner.owner_id == owner_id)
+        .where(model_game.GameOwner.game_id == game_id)
     )
     return session.exec(statement).first() is not None
 
 
-def get_game_owners(session: Session, game_id: int) -> Sequence[player.Player]:
+def get_game_owners(session: Session, game_id: int) -> Sequence[model_player.Player]:
     statement = (
-        select(player.Player)
-        .join(gameowner.GameOwner, gameowner.GameOwner.owner_id == player.Player.id)  # type: ignore[arg-type]
-        .where(gameowner.GameOwner.game_id == game_id)
+        select(model_player.Player)
+        .join(model_game.GameOwner, model_game.GameOwner.owner_id == model_player.Player.id)  # type: ignore[arg-type]
+        .where(model_game.GameOwner.game_id == game_id)
     )
     return session.exec(statement).all()
 
 
-def get_games_by_owner(session: Session, player_id: int) -> Sequence[game.Game]:
+def get_games_by_owner(session: Session, player_id: int) -> Sequence[model_game.Game]:
     statement = (
-        select(game.Game)
-        .join(gameowner.GameOwner, gameowner.GameOwner.game_id == game.Game.id)  # type: ignore[arg-type]
-        .where(gameowner.GameOwner.owner_id == player_id)
+        select(model_game.Game)
+        .join(model_game.GameOwner, model_game.GameOwner.game_id == model_game.Game.id)  # type: ignore[arg-type]
+        .where(model_game.GameOwner.owner_id == player_id)
     )
     return session.exec(statement).all()
--- src/models/game.py
+++ src/models/game.py
@@ -27,3 +27,20 @@ class GameCreate(GameBase):
 class GameRead(GameBase):
     id: int
     owners: list[str] = []
+
+
+class GameOwnerBase(SQLModel):
+    game_id: int = Field(foreign_key="game.id")
+    owner_id: int = Field(foreign_key="player.id")
+
+
+class GameOwner(GameOwnerBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+
+class GameOwnerCreate(GameOwnerBase):
+    pass
+
+
+class GameOwnerRead(GameOwnerBase):
+    id: int
